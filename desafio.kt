2cf663993cacb206b08f5d66f9a6c619f5c9c605
Luciana Santos <105242092+Luciana2202@users.noreply.github.com>
2023-10-20 20:11:34 -0300

    Update desafio.kt
    Resposta do desafio de projeto.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,50 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+import kotlin.time.Duration.Companion.minutes
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
+    private val inscritos = mutableListOf<Usuario>()
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(usuario: Usuario) { 
+        inscritos.add(usuario) }
+
+    fun imprimirMatriculado() {
+        println("ALunos Matriculados")
+        inscritos.sortBy { it.nome }
+        inscritos.forEachIndexed { index, inscrito ->
+            println("${index + 1} - ${inscrito.nome}")
+        }
+    }
+
+    fun imprimirFormacao() {
+        println("Formação")
+        println("Nome: $nome \n")
+    }
+    fun imprimirConteudosFormacao() {
+        println("Conteúdo do Programa")
+        conteudos.forEach { conteudo ->
+            println("Nome: ${conteudo.nome} \nDuração: ${conteudo.duracao.minutes} \nNível: ${conteudo.nivel} \n")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudos = listOf(
+        ConteudoEducacional(nome = "Introdução ao Kotlin", duracao = 20, nivel = Nivel.BASICO),
+        ConteudoEducacional(nome = "Resolvendo desafios com Kotlin", duracao = 120, nivel = Nivel.INTERMEDIARIO),
+        ConteudoEducacional(nome = "Primeiros Passos no Android Studio", duracao = 120, nivel = Nivel.DIFICIL)
+    )
+    val formacao = Formacao("Mobile Android com Kotlin", conteudos)
+    val usuario = Usuario("João Humberto")
+    val usuario2 = Usuario("Marina Barreto")
+
+    formacao.matricular(usuario)
+    formacao.matricular(usuario2)
+    formacao.imprimirFormacao()
+    formacao.imprimirConteudosFormacao()
+    formacao.imprimirMatriculado()
 }
